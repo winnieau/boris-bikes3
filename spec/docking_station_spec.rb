0730d4ef1bdb6ccc44048f01d22686aa27a96bdb
Winifred Au <winifredau1990@gmail.com>
2015-07-22 10:09:31 -0400

    added up to challenge 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,5 +3,11 @@ require 'docking_station'
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
 
-  
+describe Bike do
+  it {is_expected.to respond_to :working?}
+
+
+
+
+  end
 end
