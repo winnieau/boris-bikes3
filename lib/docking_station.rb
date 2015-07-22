5b74d09fee33dcbf600d945423d5ecafcc61882f
Winifred Au <winifredau1990@gmail.com>
2015-07-22 11:59:31 -0400

    added post-challenge 2

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,12 +1,12 @@
 require_relative 'bike'
 class DockingStation
+  def dock bike
+    fail 'Docking station full' if @bike
+    @bike = bike
+  end
+
   def release_bike
     fail 'No bikes available' unless @bike
     @bike
   end
-  
-  def dock bike
-    @bike = bike
-  end
-    
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,24 +1,30 @@
 require 'docking_station'
 
 describe DockingStation do
-       it {is_expected.to respond_to(:dock).with(1).argument}
-  
-      it {is_expected.to respond_to :release_bike}
+  it {is_expected.to respond_to(:dock).with(1).argument}
+
+  it {is_expected.to respond_to :release_bike}
 
   it 'releases working bikes' do
     subject.dock Bike.new
     bike = subject.release_bike
     expect(bike).to be_working
-  
-  end   
-#describe Bike do
- # it {is_expected.to respond_to :working?}
+  end
 
-
-  describe '#release_bike' do
-    it 'raises an error when there are no bikes available' do
-      expect {subject.release_bike}.to raise_error 'No bikes available'
-    end
+describe '#dock' do
+  it 'raises an error when full' do
+  subject.dock Bike.new
+  expect {subject.dock Bike.new}.to raise_error 'Docking station full'
   end
 end
 
+
+
+describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      expect {subject.release_bike}.to raise_error 'No bikes available'
+
+
+    end
+  end
+end
