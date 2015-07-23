0787b299814814cab310ca099153af50ff40281c
Balint Nemes <squarebe03@gmail.com>
2015-07-23 11:54:31 +0100

    added 20 spaces to dockingstation

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,11 +11,11 @@ describe DockingStation do
     expect(bike).to be_working
   end
 
-describe '#dock' do
-  it 'raises an error when full' do
-  subject.dock Bike.new
-  expect {subject.dock Bike.new}.to raise_error 'Docking station full'
-  end
+describe 'dock' do
+    it 'raises an error when full' do
+      20.times { subject.dock Bike.new }
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
 end
 
 
