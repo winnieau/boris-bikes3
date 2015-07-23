0bed228e1154dd527d4c8950ba169599744e413b
Balint Nemes <squarebe03@gmail.com>
2015-07-23 16:32:13 +0100

    added report_broken function

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,5 +1,19 @@
 class Bike
+
+  def initialize
+    @broken = false
+  end
+
   def working?
     true
   end
+
+  def report_broken
+    @broken = true
+  end
+
+  def broken?
+    @broken
+  end
+
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,12 @@ require 'bike'
 
 describe Bike do
   it {is_expected.to respond_to :working?}
+
+  it 'can be reported broken' do
+  	subject.report_broken
+  	expect(subject).to be_broken
+  end
+
+  #it {is_expected.to respond_to :broken?}
+
 end
