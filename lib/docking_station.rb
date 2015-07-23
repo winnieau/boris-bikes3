ce13b6b52c50734a3093274dd4e5da0a5de14552
Balint Nemes <squarebe03@gmail.com>
2015-07-23 15:40:08 +0100

    made default value changeable

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,7 @@
 require_relative 'bike'
 class DockingStation
   DEFAULT_CAPACITY = 20
-  attr_reader :capacity
+  attr_accessor :capacity
   def initialize
   	@bikes = []
     @capacity = DEFAULT_CAPACITY
