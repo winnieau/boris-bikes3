7f9ab52820103984fdceee4ae119f25a15a5a947
Edwardyao123 <zhigang_edward@hotmail.com>
2015-07-24 12:00:03 +0100

    add rspec fore not releasing a broken bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -29,7 +29,7 @@ describe '#release_bike' do
       bike = Bike.new
       bike.report_broken
       subject.dock bike
-      expect {subject.release_bike}.to raise_error 'No bikes available'
+      expect {subject.release_bike}.to raise_error 'No bikes available' 
     end
   end
 end
